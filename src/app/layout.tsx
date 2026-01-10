6a89b388cc881b6d4f29df8585e20a288adacd40
CharlieHelps <charlie@charlielabs.ai>
2026-01-10 14:39:22 +0000

    refactor(app): extract GTM snippet strings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import "./globals.css";
 
 const GTM_ID = "GTM-NG943PNQ";
 
+const GTM_BOOTSTRAP_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`;
+
+const GTM_NO_SCRIPT = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+
 export const metadata = {
   title: "Charlie technicals",
   description: "Market technicals pipeline driven by Charlie playbooks"
@@ -19,19 +23,12 @@ export default function RootLayout(props: { children: ReactNode }) {
           id="google-tag-manager"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`
+            __html: GTM_BOOTSTRAP_SCRIPT
           }}
         />
       </head>
       <body className="rpAppShell">
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+        <noscript dangerouslySetInnerHTML={{ __html: GTM_NO_SCRIPT }} />
         <main className="rpAppMain">
           <div className="rpAppContent">{props.children}</div>
         </main>
